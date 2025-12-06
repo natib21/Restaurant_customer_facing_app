46ca3aa9edd26c27dc0deffb6a6376e8861946d9
YohannesGetahunSimegn <ygetahun37@gmail.com>
2025-12-06 14:17:41 +0300

    cart fixed

--- src/features/menu/components/Header.jsx
+++ src/features/menu/components/Header.jsx
@@ -1,7 +1,7 @@
 export default function Header({ restaurantName, totalItems, onCartOpen }) {
   return (
     <header className="sticky top-0 z-30 bg-white border-b border-gray-100 shadow-sm p-3 sm:p-4 md:p-5 mb-6 md:mb-8 lg:mb-10 rounded-lg">
-      <div className="flex flex-col gap-2 sm:gap-3 md:flex-row md:items-center md:justify-between md:gap-6">
+      <div className="flex flex-row gap-2 sm:gap-3 md:flex-row md:items-center md:justify-between md:gap-6">
 
         {/* Restaurant Name */}
         <div className="flex-1 min-w-0">
@@ -17,12 +17,12 @@ export default function Header({ restaurantName, totalItems, onCartOpen }) {
         {totalItems > 0 && (
           <button
             onClick={onCartOpen}
-            className="flex items-center gap-2 bg-linear-to-r from-amber-400 to-amber-500 hover:from-amber-500 hover:to-amber-600 text-gray-900 px-3 md:px-4 py-2 md:py-2.5 rounded-lg font-semibold text-sm md:text-base transition-all duration-200 shadow-md hover:shadow-lg whitespace-nowrap shrink-0"
+            className="inline-flex w-auto max-w-fit  items-center gap-2 bg-linear-to-r from-amber-400 to-amber-500 hover:from-amber-500 hover:to-amber-600 text-gray-900 px-3 md:px-4 py-2 md:py-2.5 rounded-lg font-semibold text-sm md:text-base transition-all duration-200 shadow-md hover:shadow-lg whitespace-nowrap shrink-0"
           >
             {/* Cart Icon */}
             <svg
               xmlns="http://www.w3.org/2000/svg"
-              className="h-4 w-4 md:h-5 md:w-5"
+              className="h-4 w-4  md:h-5 md:w-5"
               fill="currentColor"
               viewBox="0 0 24 24"
             >
